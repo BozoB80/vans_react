82bdecc50e25355786fbc1cf217f5737c5a14fcd
BBakula <bozo.bakula@gmail.com>
2023-02-24 16:00:16 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,27 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import About from './pages/About'
 import Home from './pages/Home'
+import Vans from './pages/Vans'
+import VanDetail from './pages/VanDetail'
+
+import "../server"
 
 function App() {
-
+  
   return (
     <BrowserRouter>
-      <Link>
-      </Link>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
+        <Route path='/vans/:id' element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/VanDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VanDetail = () => {
+  return (
+    <div>
+      Van Detail
+    </div>
+  )
+}
+
+export default VanDetail
